f23bbd4f4f11cec2497baabb968ef5e9c6a226f5
ubrmnz <zxvf@tutanota.com>
2023-02-28 11:49:55 +0530

    feat: mvoing to typescript

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # btconnect-node
 btconnect tool written in nodejs
+
+@TODO: 
+    -[] migrate to typescript.
+    -[] option to disonnect also.
+    -[] write test maybe
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import { exec } from "node:child_process";
+import { exit } from "node:process";
 import * as readLine from "node:readline/promises";
 import { stdin as input, stdout as output } from 'process'
 
 const checkIfPackagesAreInstalled = () => {
     exec("bluetoothctl --v", (error, stdout, stderr) => {
-        console.log("\t Namaste 🙏, Welcome to btconnect 🎧");
+        console.log(`\t 
+                    █▄▄ ▀█▀ █▀▀ █▀█ █▄░█ █▄░█ █▀▀ █▀▀ ▀█▀
+                    █▄█ ░█░ █▄▄ █▄█ █░▀█ █░▀█ ██▄ █▄▄ ░█░\n`);
         if (stderr) {
             console.log(`You don't have bluetoothctl installed, Please install it using
                 your package manager to continue`);
-
         } else if (stdout) {
             console.log("~~~ Continuing to check if cut is installed ~~~");
             exec("cut --v", (error, stdout, stderr) => {
@@ -16,8 +18,8 @@ const checkIfPackagesAreInstalled = () => {
                     console.log(`cut is not installed, Please install it 
                         using your package manager to continue`)
                 } else if (stdout) {
-                    listBluetoothDevices();
                     console.log("..... Everything seems to be fine .....");
+                    getOptions();
                 } else {
                     console.log("Something happened", error);
                 }
@@ -26,6 +28,19 @@ const checkIfPackagesAreInstalled = () => {
             console.log("There's something wrong, please check logs", error);
         }
     })
+}
+
+const getOptions = async () => {
+    const rl = readLine.createInterface({ input, output });
+    const selectedOption = await rl.question(`You have to following options \n 1. Connect \n 2. Disconnect \n => \t`);
+    if(Number(selectedOption) !== 1 && Number(selectedOption) !== 2) {
+        console.error(`\t Not a valid option => ${selectedOption}! \n Please choose between \n 1. Connect \n 2. Disconnect, to continue \n => \t`)
+    } else if (Number(selectedOption) === 1){
+        listBluetoothDevices();
+    } else {
+        console.log(`Yet to implement ${selectedOption}`);
+        exit(0);
+    }
 }
 
 checkIfPackagesAreInstalled()
@@ -55,7 +70,12 @@ function listBluetoothDevices() {
 
 }
 
-async function showOptions (options) {
+type Options = {
+    optionId: number | string,
+    optionName: string
+}
+
+async function showOptions(options: Options[]) {
     const rl = readLine.createInterface({ input, output });
     const option = await rl.question(`\t You have ${options.length} devices to connect to \n ${options.map((opt, index) => `${index}. ${opt.optionName} \n`)}`);
     rl.close();
@@ -63,7 +83,7 @@ async function showOptions (options) {
 }
 
 
-function connectToBt (input, options) {
+function connectToBt(input: string | number, options: Options[]) {
     const choosenOption = options[input];
     console.log(`Connecting to ${choosenOption["optionName"]} 🎧`);
     exec(`bluetoothctl connect ${choosenOption["optionId"]}`, (error, stdout, stderr) => {
